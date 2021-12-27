429ed3bb90efe6e21a56c9b0cdd3601c38a300bc
Sasha <Chorniy2008@ukr.net>
2021-12-27 22:54:39 +0200

    made second type of slider

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', e => {
     /**
      * timer
      */
-    const deadLine = '2021-12-25';
+    const deadLine = '2022-12-25';
     function getTimeRemaining(endtime){
         let t = Date.parse(endtime) - Date.parse(new Date());
         let days = Math.floor(t/(1000*60*60*24));
@@ -176,6 +176,7 @@ window.addEventListener('DOMContentLoaded', e => {
             this.parent.append(element);
         }
     }
+
     const getResources = async (url) => {
         const res = await fetch(url);
         if(!res.ok){
@@ -259,6 +260,96 @@ window.addEventListener('DOMContentLoaded', e => {
         }, 4000);
     }
 
-    fetch('http://localhost:3000/menu').then(data => data.json()).then(res => console.log(res));
+    /**
+     * Slider
+     */
+    const slides = document.querySelectorAll('.offer__slide');
+    const prev = document.querySelector('.offer__slider-prev');
+    const next = document.querySelector('.offer__slider-next');
+    const total = document.querySelector('#total');
+    const current = document.querySelector('#current');
+    const slidesWrapper = document.querySelector('.offer__slider-wrapper');
+    const slidesField = document.querySelector('.offer__slider-inner');
+    const width = window.getComputedStyle(slidesWrapper).width;
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+    slidesWrapper.style.overflow = 'hidden';
+    slides.forEach(el => el.style.width = width);
+    let offset = 0;
+    let slideIndex = 1;
+    if (slideIndex < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length.toString();
+        current.textContent = slideIndex;
+    }
+    next.addEventListener('click', e => {
+        if(offset === +width.slice(0,width.length - 2) * (slides.length - 1)){
+            offset = 0;
+        } else {
+            offset += +width.slice(0,width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+        if(slideIndex === slides.length){
+            slideIndex = 1;
+        }else{
+            slideIndex++;
+        }
+        if(slides.length < 10){
+            current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex;
+        }
+    });
+    prev.addEventListener('click', e => {
+        if(offset == 0){
+            offset = +width.slice(0,width.length - 2) * (slides.length + 1);
+        } else {
+            offset -= +width.slice(0,width.length - 2);
+        }
+        slidesField.style.transform = `translateX(+${offset}px)`;
+        if(slideIndex == 1){
+            slideIndex = slides.length;
+        }else{
+            slideIndex--;
+        }
+        if(slides.length < 10){
+            current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex;
+        }
+    });
 
+    // showSlides(slideIndex);
+    // if(slideIndex < 10){
+    //     total.textContent = `0${slides.length}`;
+    // } else {
+    //     total.textContent = slides.length.toString();
+    // }
+    // function showSlides(n) {
+    //     if(n > slides.length){
+    //         slideIndex = 1;
+    //     }
+    //     if(n < 1){
+    //         slideIndex = slides.length;
+    //     }
+    //     slides.forEach(el => el.classList.add('hide','fade'));
+    //     slides[slideIndex - 1].classList.remove('hide');
+    //     if(slideIndex < 10){
+    //         current.textContent = `0${slideIndex}`;
+    //     } else {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+    // function plusSlides(n) {
+    //     showSlides(slideIndex += n);
+    // }
+    // prev.addEventListener('click', () => {
+    //     plusSlides(-1);
+    // });
+    // next.addEventListener('click', () => {
+    //     plusSlides(+1);
+    // });
 });
